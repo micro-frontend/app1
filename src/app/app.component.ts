31d015af1161f5504ae31cd7cd38f52c87a0465b
Zhicheng Wang <zcwang@thoughtworks.com>
2018-02-26 19:57:28 +0800

    feat: "no routing" version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-app1',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  count = 0;
+
+  increase(): void {
+    this.count++;
+  }
 }
